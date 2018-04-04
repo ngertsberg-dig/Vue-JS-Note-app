4d1673a288e988e778f99a5206974f5fe1d72bae
nickey22 <gertsberg@hotmail.com>
2018-04-04 14:27:07 -0400

    init

---
+++ js/index.js
@@ -0,0 +1,45 @@
+$(".input-code-container").hide();
+      var app1 = new Vue({
+        el:"#app",
+        data:{
+          notes:[
+            {title:"Title 1 sample title",message:`data:{
+            THIS IS A SAMPLE NOTE notes:[{title:"Title 1",message:"THIS IS A SAMPLE NOTE"}],
+            showHome:true,
+            showNote:false
+          },THIS IS A SAMPLE NOTE`}
+        ],
+          showHome:true,
+          showNote:false
+        },
+        methods:{
+          togglePages(){
+            if(this.showHome == true && this.showNote ==false){
+            this.showHome = false
+            this.showNote = true
+            $(".input-code-container").fadeIn()
+            $(".note-list").hide()
+
+            }
+            else{
+              this.showHome = true
+              this.showNote = false
+                    $(".input-code-container").hide()
+                    $(".note-list").fadeIn()
+            }
+          },
+          submitNote(){
+                this.notes.push({title:$(".title-input").val(),message:$("textarea").val()});
+                $("textarea").val("");
+                $(".back-home i").click();
+              },
+              clearInputTitle(){
+                $('.title-input').val('');
+                $('.title-input').css({'textAlign':'left','paddingLeft':'5px'})
+              },
+              deleteNote(e){
+                var noteClicked = e.target;
+                $(noteClicked).parent().parent().parent().remove();
+              }
+            }
+        })
